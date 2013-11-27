d439128adf382a7f7de38f238ef24283088b6910
vinaud <leyenasd@gmail.com>
2013-11-27 17:23:30 -0200

    Update and rename teste.java to Teste.java

--- Engajamundo/src/testes/Teste.java
+++ Engajamundo/src/testes/Teste.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class teste {
+public class Teste {
 
 	@Test
 	public void test() {
-		Engajador engajador1 = new Engajador("Joseph","Décimo","joseph@teste.teste","01/01/0001",12345678,"galileia","joseph","senha");
+		Engajador engajador1 = new Engajador("Joseph","Decimo","joseph@teste.teste","01/01/0001",12345678,"galileia","joseph","senha");
 		assertEquals("Joseph", engajador1.getNome());
 	}
 
