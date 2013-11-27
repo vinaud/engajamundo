f129cc226fae701e765aea0c4cb4fb6dc813f091
vinaud <vinaud@LCC14.lcc.ufrn.br>
2013-11-27 16:08:13 -0300

    Primeiro teste bem básico para uso do CoverAlls

---
+++ Engajamundo/src/testes/teste.java
@@ -0,0 +1,15 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class teste {
+
+	@Test
+	public void test() {
+		Engajador engajador1 = new Engajador("Joseph","Décimo","joseph@teste.teste","01/01/0001",12345678,"galileia","joseph","senha");
+		assertEquals("Joseph", engajador1.getNome());
+	}
+
+}
