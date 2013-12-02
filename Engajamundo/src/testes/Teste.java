3d8a5b39cc6fd706da9f14e9d512bb36255787a3
vinaud <leyenasd@gmail.com>
2013-12-02 10:03:28 -0200

    Update Teste.java
    Testando. Agora o coveralls já reconhece os testes, graças as bençãos de Hyllia

--- Engajamundo/src/testes/Teste.java
+++ Engajamundo/src/testes/Teste.java
@@ -12,6 +12,9 @@ public class Teste {
 	public void test() {
 		Engajador engajador1 = new Engajador("Joseph","Decimo","joseph@teste.teste","01/01/0001",12345678,"galileia","joseph","senha");
 		assertEquals("Joseph", engajador1.getNome());
+		assertEquals("Decimo", engajador1.getSobrenome());
+		assertEquals("joseph@teste.teste", engajador1.getEmail());
+		
 	}
 
 }
