f5e4573686ff7067645c805b3500375bcacad897
vinaud <leyenasd@gmail.com>
2013-12-03 09:21:13 -0200

    Create FormPaisesTeste.java

---
+++ Engajamundo/src/testes/FormPaisesTeste.java
@@ -0,0 +1,22 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import controller.FormPaises;
+
+public class FormPaisesTeste {
+
+	@Test
+	public void test() {
+		FormPaises f = new FormPaises();
+		List<String> selectedMovies = null;
+		selectedMovies.add("Chile");
+		f.setSelectedPaises(selectedMovies);
+		
+	}
+
+}
