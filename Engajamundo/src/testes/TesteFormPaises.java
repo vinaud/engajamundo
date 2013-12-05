af615be0859a8b36fcec4cc466537d95f2d1d636
vinaud <leyenasd@gmail.com>
2013-12-05 12:03:06 -0200

    Update TesteFormPaises.java

--- Engajamundo/src/testes/TesteFormPaises.java
+++ Engajamundo/src/testes/TesteFormPaises.java
@@ -20,5 +20,40 @@ public class TesteFormPaises {
 		
 		
 	}
+	
+	 @Test
+    public void testGetSelectedPaises() {
+        System.out.println("getSelectedPaises");
+        FormPaises instance = new FormPaises();
+        List expResult = null;
+        List result = instance.getSelectedPaises();
+        assertEquals(expResult, result);
+       
+    }
+
+    /**
+     * Test of setSelectedPaises method, of class FormPaises.
+     */
+    @Test
+    public void testSetSelectedPaises() {
+        System.out.println("setSelectedPaises");
+        List<String> selectedMovies = null;
+        FormPaises instance = new FormPaises();
+        instance.setSelectedPaises(selectedMovies);
+       
+    }
+
+    /**
+     * Test of getPaises method, of class FormPaises.
+     */
+    @Test
+    public void testGetPaises() {
+        System.out.println("getPaises");
+        FormPaises instance = new FormPaises();
+        Map expResult = null;
+        Map result = instance.getPaises();
+        assertEquals(expResult, result);
+        
+    }
 
 }
