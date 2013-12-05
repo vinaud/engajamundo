2e00fbbab194e67ed8ed01d614f76859904c2e58
vinaud <leyenasd@gmail.com>
2013-12-05 09:04:40 -0200

    Update ServicoUsuarioTeste.java

--- Engajamundo/src/testes/ServicoUsuarioTeste.java
+++ Engajamundo/src/testes/ServicoUsuarioTeste.java
@@ -12,26 +12,20 @@ import exceptions.CadastroFailException;
 public class ServicoUsuarioTeste {
 
 	@Test
-	public void testLogin() {
+	public void testLogin() throws LoginInvalidoException {
 		ServicoUsuario ser = new ServicoUsuario();
-		try {
+		
 			ser.autenticar("usuario", "senha");
-		} catch (LoginInvalidoException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
 	}
 	
 	@Test
-	public void testCadastro() {
+	public void testCadastro() throws LoginInvalidoException{
 		ServicoUsuario ser = new ServicoUsuario();
 		Engajador eng = new Engajador();
-		try {
+		
 			ser.cadastrarEngajador(eng);
-		} catch (CadastroFailException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		
 		
 	}
