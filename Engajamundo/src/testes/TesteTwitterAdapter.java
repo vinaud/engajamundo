209b57e8a24d97b2dc7833cb47621198a4bdef71
vinaud <leyenasd@gmail.com>
2013-12-05 09:57:09 -0200

    Update TesteTwitterAdapter.java

--- Engajamundo/src/testes/TesteTwitterAdapter.java
+++ Engajamundo/src/testes/TesteTwitterAdapter.java
@@ -18,40 +18,7 @@ public class TesteTwitterAdapter {
     public TesteTwitterAdapter() {
     }
 
-    @org.junit.BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @org.junit.AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @org.junit.Before
-    public void setUp() throws Exception {
-    }
-
-    @org.junit.After
-    public void tearDown() throws Exception {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-        
-        TwitterAdapter ta = new TwitterAdapter();
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    
+   
      @Test
      public void testGetTweets() throws TwitterException {
           TwitterAdapter ta = new TwitterAdapter();
